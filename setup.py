dca505f5b13cdaff5b7bf55352156ea415d02c2a
Oleg Utkin <oleg.utkin@uber.com>
2018-12-19 16:18:07 -0800

    Prepare for submission

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="dotter",
-    version="0.0.1dev",
+    version="0.0.1",
+    description="A dotfile link farm manager",
+    author="nonlogicaldev",
+    url="https://github.com/NonLogicalDev/nld.cli.dotter",
+
     packages=find_packages(),
     scripts=["bin/dotter"]
 )
